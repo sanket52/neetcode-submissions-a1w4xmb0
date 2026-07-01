bd22942597b6b513588d2ea124a05aad6dfe0a3b
sanket  sharma <144638378+sanket52@users.noreply.github.com>
2026-07-01 10:23:17 +0530

    Add: valid-palindrome-ii - submission-5

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-5.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public boolean Palidrome(String s,int left,int right)
+    {
+        while(left<right)
+        {
+              if(s.charAt(left)!=s.charAt(right))
+        {
+            return false;
+        }
+
+        left++;
+        right--;
+
+        }
+      
+      return true;
+    }
+
+
+    public boolean validPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+
+        while(left<right)
+        {
+            if(s.charAt(left)!=s.charAt(right))
+            {
+                return Palidrome(s,left+1,right)||Palidrome(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+          return true;
+    }
+  
+}
